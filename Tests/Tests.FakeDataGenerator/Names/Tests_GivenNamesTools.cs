c06eaf9fb4ebfcc4845ae669e7a46eca6df9ec6e
Ivan Perehynets <jonilviv@gmail.com>
2018-06-26 16:40:14 +0200

    Added unit tests.

---
+++ Tests/Tests.FakeDataGenerator/Names/Tests_GivenNamesTools.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using FakeDataGenerator.Names;
+using NUnit.Framework;
+
+namespace Tests.FakeDataGenerator.Names
+{
+    [TestFixture]
+    public sealed class Tests_GivenNamesTools
+    {
+        [Test]
+        public void Test_GivenNamesTools__GivenNames_returns_english_names_with_both_sex()
+        {
+            //Arrange
+            CultureInfo ci = CultureInfo.GetCultureInfo("en");
+
+
+            //Act
+            List<GivenName> names = GivenNamesTools.GivenNames;
+
+
+            //Assert
+            IEnumerable<GivenName> enNames = names.Where(x => Equals(x.Culture, ci));
+            Assert.IsNotNull(enNames);
+            Assert.IsNotEmpty(enNames);
+
+            IEnumerable<GivenName> enMaleNames = enNames.Where(x => x.Sex == Sex.Male);
+            Assert.IsNotNull(enMaleNames);
+            Assert.IsNotEmpty(enMaleNames);
+
+            IEnumerable<GivenName> enFemaleNames = enNames.Where(x => x.Sex == Sex.Female);
+            Assert.IsNotNull(enFemaleNames);
+            Assert.IsNotEmpty(enFemaleNames);
+        }
+
+        [Test]
+        public void Test_GivenNamesTools__Culture_returns_english_names_with_both_sex()
+        {
+            //Arrange
+            CultureInfo ci = CultureInfo.GetCultureInfo("en");
+
+
+            //Act
+            IEnumerable<GivenName> names = GivenNamesTools.GivenNames.Culture(ci);
+
+
+            //Assert
+            IEnumerable<GivenName> enNames = names.Where(x => Equals(x.Culture, ci));
+            Assert.IsNotNull(enNames);
+            Assert.IsNotEmpty(enNames);
+
+            IEnumerable<GivenName> enMaleNames = enNames.Where(x => x.Sex == Sex.Male);
+            Assert.IsNotNull(enMaleNames);
+            Assert.IsNotEmpty(enMaleNames);
+
+            IEnumerable<GivenName> enFemaleNames = enNames.Where(x => x.Sex == Sex.Female);
+            Assert.IsNotNull(enFemaleNames);
+            Assert.IsNotEmpty(enFemaleNames);
+        }
+
+        [Test]
+        public void Test_GivenNamesTools__Culture_returns_english_names_with_both_sex_()
+        {
+            //Arrange
+            CultureInfo ci = CultureInfo.GetCultureInfo("en");
+
+
+            //Act
+            IEnumerable<GivenName> names = GivenNamesTools.GivenNames.Culture("en");
+
+
+            //Assert
+            IEnumerable<GivenName> enNames = names.Where(x => Equals(x.Culture, ci));
+            Assert.IsNotNull(enNames);
+            Assert.IsNotEmpty(enNames);
+
+            IEnumerable<GivenName> enMaleNames = enNames.Where(x => x.Sex == Sex.Male);
+            Assert.IsNotNull(enMaleNames);
+            Assert.IsNotEmpty(enMaleNames);
+
+            IEnumerable<GivenName> enFemaleNames = enNames.Where(x => x.Sex == Sex.Female);
+            Assert.IsNotNull(enFemaleNames);
+            Assert.IsNotEmpty(enFemaleNames);
+        }
+
+        [Test]
+        public void Test_GivenNamesTools__Random_empty_list_returns_null_GivenName()
+        {
+            //Arrange
+            //Act
+            GivenName name = new List<GivenName>().Random();
+
+
+            //Assert
+            Assert.Null(name);
+        }
+
+        [Test]
+        public void Test_GivenNamesTools__Random_returns_some_name()
+        {
+            //Arrange
+            //Act
+            GivenName name = GivenNamesTools.GivenNames.Random();
+
+
+            //Assert
+            Assert.IsNotNull(name);
+            Assert.IsNotNull(name.Sex);
+            Assert.IsNotNull(name.Culture);
+            Assert.IsNotNull(name.Name);
+            Assert.IsNotEmpty(name.Name);
+        }
+
+    }
+}
---
+++ Tests/Tests.FakeDataGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests.FakeDataGenerator")]
+[assembly: AssemblyDescription("Unit test for FakeDataGenerator")]
+[assembly: AssemblyProduct("Tests.FakeDataGenerator")]
+[assembly: AssemblyCopyright("Ivan Perehynets")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ee641a0e-16ac-4d31-9846-06a940e931e3")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
