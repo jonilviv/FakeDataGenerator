ac01cebc17d2dfa0de8d091fe6acffc76f492b8b
Ivan Perehynets <jonilviv@gmail.com>
2018-08-03 09:30:44 +0200

    Added global attribute for run all tests in project in parallel mode.

--- Tests/Tests.FakeDataGenerator/Properties/AssemblyInfo.cs
+++ Tests/Tests.FakeDataGenerator/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
+using NUnit.Framework;
 
 [assembly: AssemblyTitle("Tests.FakeDataGenerator")]
 [assembly: AssemblyDescription("Unit test for FakeDataGenerator")]
@@ -9,3 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("ee641a0e-16ac-4d31-9846-06a940e931e3")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: LevelOfParallelism(16)]
