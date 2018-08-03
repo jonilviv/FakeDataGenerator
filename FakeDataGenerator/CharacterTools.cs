83b1316506715a0caec09244b970097731047ba6
Ivan Perehynets <jonilviv@gmail.com>
2018-08-03 13:44:16 +0200

    Added class `CharacterTools`.

---
+++ FakeDataGenerator/CharacterTools.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace FakeDataGenerator
+{
+    public static class CharacterTools
+    {
+        public static UnicodeCategoryCharacter[] UnicodeCategoryCharacters { get; } = new UnicodeCategoryCharacter[ushort.MaxValue];
+
+        static CharacterTools()
+        {
+            for (int i = 0; i < ushort.MaxValue; i++)
+            {
+                char c = (char)i;
+                UnicodeCategory uc = char.GetUnicodeCategory(c);
+                var ucc = new UnicodeCategoryCharacter(c, uc);
+                UnicodeCategoryCharacters[i] = ucc;
+            }
+        }
+    }
+}
