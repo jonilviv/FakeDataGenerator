cda32a992309d501aa5e8d5b9531d55a897fa3c0
Ivan Perehynets <jonilviv@gmail.com>
2018-08-03 13:49:31 +0200

    Added class `CharacterTools`.

---
+++ FakeDataGenerator/UnicodeCategoryCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace FakeDataGenerator
+{
+    public class UnicodeCategoryCharacter
+    {
+        public UnicodeCategoryCharacter(char chr, UnicodeCategory unicodeCategory)
+        {
+            Char = chr;
+            UnicodeCategory = unicodeCategory;
+        }
+
+        public char Char { get; }
+
+        public UnicodeCategory UnicodeCategory { get; }
+    }
+}
